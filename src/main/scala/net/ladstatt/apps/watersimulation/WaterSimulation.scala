b4093b4cd510a016ed3a104c48083536d3e2ea0d
Robert Ladstätter <robert@ladstaetter.info>
2013-01-06 23:01:18 +0100

    creating a canvas with writeable image

--- src/main/scala/net/ladstatt/apps/watersimulation/WaterSimulation.scala
+++ src/main/scala/net/ladstatt/apps/watersimulation/WaterSimulation.scala
@@ -12,13 +12,16 @@ import javafx.stage.Stage
 import java.net.URL
 import java.util.ResourceBundle
 import javafx.scene.Group
+import javafx.scene.image.ImageView
+import javafx.scene.image.WritableImage
+import javafx.scene.paint.Color
 
 /**
  * A simple application which demonstrates a water simulation in javafx.
- * 
+ *
  * Based on the ideas of http://gamedev.tutsplus.com/tutorials/implementation/make-a-splash-with-2d-water-effects/
- * 
- * See also blog post 
+ *
+ * See also blog post
  */
 object WaterSimulation {
 
@@ -28,13 +31,31 @@ object WaterSimulation {
 
 }
 
-
 class WaterSimulation extends javafx.application.Application {
+
+  val imgWidth = 1024
+  val imgHeight = 768
+
+  def randColor = Color.rgb(0, 0, 0)
+
+  def createImage(): ImageView = {
+    val imageView = new ImageView()
+    val writeableImage = new WritableImage(imgWidth, imgHeight)
+    val pw = writeableImage.getPixelWriter()
+    for {
+      x <- 0 to imgWidth - 1
+      y <- 0 to imgHeight - 1
+    } pw.setColor(x, y, randColor)
+
+    imageView.setImage(writeableImage)
+    imageView
+  }
 
   override def start(primaryStage: Stage): Unit = {
     primaryStage.setTitle("Water Simulation");
-    val root = new Group()
-    primaryStage.setScene(new Scene(root, 600,400))
+    val root = new StackPane()
+    root.getChildren.add(createImage)
+    primaryStage.setScene(new Scene(root, imgWidth, imgHeight))
     primaryStage.show()
   }
 
